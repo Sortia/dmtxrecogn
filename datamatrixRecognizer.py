b3acdbb47ad2059769a3fc463a91f44f94dcce0c
ssortia <alexkiyan.lug@gmail.com>
2023-11-30 13:10:40 +0300

    improve recognition quality

--- datamatrixRecognizer.py
+++ datamatrixRecognizer.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 from pylibdmtx.pylibdmtx import decode
 
 
@@ -6,8 +7,8 @@ class DatamatrixRecognizer:
     def __init__(self, path, segment):
         self.resized_height = 2400
         self.resized_width = None
-        self.zone_scale_x = 2.5
-        self.zone_scale_y = 2.5
+        self.zone_scale_x = 2
+        self.zone_scale_y = 2
         self.bordered = None
 
         self.timeout = 3
@@ -24,22 +25,18 @@ class DatamatrixRecognizer:
     def process_img(img):
         img = cv2.convertScaleAbs(img, alpha=1.5, beta=30)
 
-        clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
-        img = clahe.apply(img)
-
         _, img = cv2.threshold(img, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+        kernel = np.ones((25, 25), np.uint8)
+        closed = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
+        img[closed == 0] = 255
 
         return img
 
     def resize_img(self, img):
-        # Определение текущих размеров изображения
         height, width = img.shape[:2]
-
-        # Расчет пропорциональной ширины на основе желаемой высоты
         aspect_ratio = width / height
         self.resized_width = int(self.resized_height * aspect_ratio)
 
-        # Изменение размера изображения
         return cv2.resize(img, (self.resized_width, self.resized_height))
 
     def crop_img(self, img):
@@ -58,44 +55,27 @@ class DatamatrixRecognizer:
         return img[y:crop_y, x:crop_x]
 
     def get_dmtx_text(self, img):
-        data = decode(img, timeout=self.timeout * 1000, max_count=1)
+        data = decode(img, timeout=self.timeout * 1000, max_count=1, corrections=10)
 
         if data:
             return data[0].data.decode('utf-8')
         else:
-            return None
+            return '-'
 
-    def add_border(self, img):
-        top, bottom, left, right = 0, 0, 0, 0
-        border_width = 100
-
-        if self.segment.y == 1:
-            top = border_width
-
-        if self.segment.y == self.count_y_segments:
-            bottom = border_width
-
-        if self.segment.x == 1:
-            left = border_width
-
-        if self.segment.x == self.count_x_segments:
-            right = border_width
-
-        self.resized_height += top + bottom
-        self.resized_width += left + right
-
-        return cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=(255, 255, 255))
+    @staticmethod
+    def add_borders(img):
+        return cv2.copyMakeBorder(img, 150, 150, 150, 150, cv2.BORDER_CONSTANT, value=(255, 255, 255))
 
     def prepare_img(self, img):
         img = self.resize_img(img)
-        # img = self.add_border(img)
         img = self.crop_img(img)
         img = self.process_img(img)
+        img = self.add_borders(img)
 
         return img
 
     def magick(self):
         img = self.get_img(self.path)
         img = self.prepare_img(img)
-        cv2.imwrite("test.png", img)
+        # cv2.imwrite("test.png", img)
         return self.get_dmtx_text(img)
